a577b0d686ad7d6ba8e2cda3ac45095a0e1d3891
sgkens <sgkens@lab.davilion.online>
2023-12-07 12:47:58 +1100

    ✅ test: Add tests.

---
+++ test/pester-unit-test.ps1
@@ -0,0 +1,22 @@
+import-module -name .\
+
+BeforeAll {
+
+}
+
+Describe "csverify.cmdlets" {
+  
+    it "New-CheckSum Should Return a string" {
+    (New-CheckSum -Path .\).GetType().Name | should -be "String"
+    }
+    it "Read-CheckSum Should Return a string" {
+    (New-CheckSum -Path .\).GetType().Name | should -be "String"
+    }
+    it "New-VerificationFile Should Return a string" {
+    (New-VerificationFile).GetType().Name | should -be "Object[]"
+    }
+    it "Test-Verification Should Return a string" {
+    (Test-Verification).GetType().Name | should -be "Object[]"
+    }
+
+}
