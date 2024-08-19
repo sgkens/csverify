b1e1c65795f97efb217439e2f55af5bde649554d
sgkens <sgkens@gmail.com>
2024-08-20 08:12:56 +1000

    🔧 chore(tools): Adding misc scripts

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,42 @@
+
+$ErrorActionPreference = 'Stop'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = ''
+$url64      = ''
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE_MSI_OR_MSU'
+  url           = $url
+  url64bit      = $url64
+
+  softwareName  = 'test*'
+
+  checksum      = ''
+  checksumType  = 'sha256'
+  checksum64    = ''
+  checksumType64= 'sha256'
+
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
